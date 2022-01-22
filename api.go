2ea2205769a27e2a01714bf82c6f7c5dad7ebbd4
LionZXY <nikita@kulikof.ru>
2022-01-22 15:07:24 +0300

    Fix tiktok request

--- api.go
+++ api.go
@@ -15,6 +15,7 @@ const Aid = 1233
 const ManifestVersionCode = "2021501031"
 const UserAgent = "com.zhiliaoapp.musically/2021501031 (Linux; U; Android 10; ru_RU; Redmi Note 7; Build/QQ1B.200205.002; Cronet/TTNetVersion:79d23018 2020-02-03 QuicVersion:ac58aac6 2020-01-20)"
 const OdinTt = "c44c45e3aa0c1faae1d6bd6f21907c0cbf48736d70367fd2c8c253444d8dd63388ad96c29933dccb2311ddd94c682b9eaea57a3bab4d025cdc95d0bc939fa714"
+const XttToken = "039fe33be5d19a6ac1daa8236f077f6236007a3a0694226f15d3bc6c2eec2123e4324b7606917e7fffd812e95ba41296ac17990beb716f13f716dcb418381c6b0b3aa00830339a0a7ff8b115c664a7664744ede8438195437f0be9d0fbc94c2f148a5-1.0.1"
 
 var secUIDReg = regexp.MustCompile(`(?m)secUid":"(.*?)"`)
 var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -49,6 +50,7 @@ func getLikedVideos(secUserID string, count int) ([]video, error) {
 		fmt.Sprintf("aid=%d&device_id=%d&sec_user_id=%s&count=%d&manifest_version_code=%s",
 			Aid, 1000000000+seededRand.Intn(1000000000), secUserID, count, ManifestVersionCode))
 	req.Header.SetUserAgent(UserAgent)
+	req.Header.Set("x-tt-token", XttToken)
 	req.Header.SetCookie("odin_tt", OdinTt)
 
 	err := fasthttp.Do(req, res)
